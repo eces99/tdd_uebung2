c942258a4249f8b1807dd785475744bd5b927a76
eces99 <eecesenn@gmail.com>
2024-06-02 17:46:41 +0200

    failing test for third toDo (ece)

--- uebung2/spec/electron_spec.js
+++ uebung2/spec/electron_spec.js
@@ -9,4 +9,8 @@ describe('Electrons', function() {
     it('should return 4 for the second array', function() {
 		expect(electron.add('[ 2, 2, 3, 3 ]')).toEqual(4);
 	});
+
+    it('should return 2 for the third array', function() {
+        expect(electron.add('[ 6, 6, 4, 4, 1, 3 ]')).toEqual(2);
+    });
 });
--- uebung2/src/electron.js
+++ uebung2/src/electron.js
@@ -1,6 +1,10 @@
 // electron.js
 function Electrons() {}
 Electrons.prototype.add = function(electron_array) {
+    // failing test for the third array
+    if (electron_array == '[ 6, 6, 4, 4, 1, 3 ]') {
+        return 3;
+    }
 
     // Refactoring
     var sum = 0;
