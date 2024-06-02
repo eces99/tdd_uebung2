225c411120aaa5b99f6f981250675f97b02cfbbe
eces99 <eecesenn@gmail.com>
2024-06-02 17:41:22 +0200

    first failing test for first toDo (ece)

--- uebung2/spec/electron_spec.js
+++ uebung2/spec/electron_spec.js
@@ -2,4 +2,8 @@ describe('Electrons', function() {
 	beforeEach(function() {
 		electron = new Electrons();
 	});
+    
+    it('should return 6 for the first array', function() {
+		expect(electron.add('[1, 2, 3, 4, 5 ]')).toEqual(6);
+	});
 });
--- uebung2/src/electron.js
+++ uebung2/src/electron.js
@@ -1,4 +1,8 @@
 // electron.js
 function Electrons() {}
 Electrons.prototype.add = function(electron_array) {
+    // first faiiling test for the first array
+    if (electron_array === '[1, 2, 3, 4, 5 ]') {
+        return 1;
+    }
 };
