823796a86072ff8c7f16acdb94dc8851c701756f
eces99 <eecesenn@gmail.com>
2024-06-02 17:41:45 +0200

    simple code for first toDo (axel)

--- uebung2/src/electron.js
+++ uebung2/src/electron.js
@@ -3,6 +3,6 @@ function Electrons() {}
 Electrons.prototype.add = function(electron_array) {
     // first faiiling test for the first array
     if (electron_array === '[1, 2, 3, 4, 5 ]') {
-        return 1;
+        return 6;
     }
 };
