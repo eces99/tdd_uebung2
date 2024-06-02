a55eb41891834a391c2bc736e217e1c1e3b8ce2b
eces99 <eecesenn@gmail.com>
2024-06-02 17:42:31 +0200

    refactoring for first toDo (ece)

--- uebung2/src/electron.js
+++ uebung2/src/electron.js
@@ -1,8 +1,23 @@
 // electron.js
 function Electrons() {}
 Electrons.prototype.add = function(electron_array) {
-    // first faiiling test for the first array
-    if (electron_array === '[1, 2, 3, 4, 5 ]') {
-        return 6;
+
+    // Refactoring for the fourth todo
+    var sum = 0;
+    var electron_array = electron_array.replace(/\[|\]/g, '').split(', ');
+    for (var i = 0; i < electron_array.length; i++) {
+        if(electron_array[i] == 1 || electron_array[i] == 2 || electron_array[i] == 4 || electron_array[i] == 6) {
+            electron_array[i] = 0;
+            sum += electron_array[i];
+        }
+        else if (electron_array[i] == 3) {
+            electron_array[i] = 2;
+            sum += electron_array[i];
+            
+        } else {
+            electron_array[i] = 4;
+            sum += electron_array[i];
+        }
     }
+    return sum;
 };
