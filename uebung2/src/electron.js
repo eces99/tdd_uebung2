5ff47817838f707c4b7796d404fa4b910d99dd5c
eces99 <eecesenn@gmail.com>
2024-06-02 17:49:41 +0200

    refactoring for fourth toDo (axel)

--- uebung2/src/electron.js
+++ uebung2/src/electron.js
@@ -2,10 +2,6 @@
 function Electrons() {}
 Electrons.prototype.add = function(electron_array) {
 
-    //failing test for the fourth array
-    if(electron_array == '[ 3, 5, 3, 5, 4, 2 ]') {
-        return 12;
-    }
     // Refactoring
     var sum = 0;
     var electron_array = electron_array.replace(/\[|\]/g, '').split(', ');
